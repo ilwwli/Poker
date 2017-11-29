2224a1873ec7ac2d9a0dccbc4fddbc92ce49562f
ZXH <863886245@qq.com>
2017-11-29 23:44:14 +0800

    Framework ver 0.01

--- Code/XXK/playersManager.py
+++ Code/XXK/playersManager.py
@@ -2,25 +2,12 @@ import card
 import player
 import cardsManager
 
-def initPlayers():
-    print '****** Code here *****'
+def init_all():
 
-def addPlayers():
-    print '****** Code here *****'
 
-def delPlayer():
-    print '****** Code here *****'
+def game():
+    reset_game()
+    while not winner:
+        NextOne()
 
-def resetPlayer():
-    print '****** Code here *****'
 
-def start():
-    print '****** Code here *****'
-    players = initPlayers()
-    cardsManager.cardDistributor(card.buildPoker(), players)
-
-def nextplayer():
-    print '****** Code here *****'
-
-def end():
-    print '****** Who is the winner ******'
