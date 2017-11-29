5f2c70d784b4e19dd0448cf1a0e3e146e0948fc6
ustcxiexk <30454548+ustcxiexk@users.noreply.github.com>
2017-11-29 21:53:25 +0800

    Add files via upload

---
+++ Code/XXK/playerManager.py
@@ -0,0 +1,26 @@
+import card
+import player
+import cardsManager
+
+def initPlayers():
+    print '****** Code here *****'
+
+def addPlayers():
+    print '****** Code here *****'
+
+def delPlayer():
+    print '****** Code here *****'
+
+def resetPlayer():
+    print '****** Code here *****'
+
+def start():
+    print '****** Code here *****'
+    players = initPlayers()
+    cardsManager.cardDistributor(card.buildPoker(), players)
+
+def nextplayer():
+    print '****** Code here *****'
+
+def end():
+    print '****** Who is the winner ******'
