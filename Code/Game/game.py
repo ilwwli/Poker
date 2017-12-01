b33425782764f769b4e3bfe40a64d35283ea75e0
ustcxiexk <30454548+ustcxiexk@users.noreply.github.com>
2017-12-01 22:16:27 +0800

    Update game.py
    不加print的实现

--- Code/Game/game.py
+++ Code/Game/game.py
@@ -13,7 +13,6 @@ class Game():
         '''
         Attributes are defined here
         Game Initialization is delegated to init_game()
-
         Attributes:
             self.board : Pubilc Decks
             self.players : List of Player Infomations
@@ -42,7 +41,6 @@ class Game():
         '''
         Parameters that will not change till the program ends
             will be initialized in this function.
-
         Paremeters that are initialized here include:
             player_numbers
             player_name for each player
@@ -56,39 +54,47 @@ class Game():
         self.pack_num = int(input("How many decks do you want to play?\n"))
 
     def next_round(self, currentPlayer):
-        lastPlayerClaim = {}
+        PassCount = 0
+        lastPlayerClaim = {}  #dict(claim_length:int, claim_rank:card_rank)
+        IsNewTurn = True  # or TurnCount
         while (True):
-            playerInfo = currentPlayer.turn(lastPlayerClaim)
-            if (playerInfo['ifWin']):
+            playerInfo = currentPlayer.turn(IsNewTurn)
+            IsNewTurn = False
+            if (playerInfo['Win']):
                 return None
-            elif (playerInfo['ifDoubt']):
-                #if (len(board.GameBoard.GetDisplayArea) == lastPlayerClaim['claim_length']) \
-                #        and (board.GameBoard.GetDisplayArea == lastPlayerClaim['claim_rank']):
-                if True:
+            elif playerInfo['Choice'] == 'Claim':
+                lastPlayerClaim = playerInfo['Claim']
+                currentPlayer.play_cards(playerInfo['Cards'])
+                board.GameBoard.Display(playerInfo['Cards'])
+            elif playerInfo['Choice'] == 'Qustion':
+                LastPlayerCards = board.GameBoard.GetDisplayArea[len(board.GameBoard.GetDisplayArea()) \
+                            - len(playerInfo['Cards']): len(board.GameBoard.GetDisplayArea())]
+                temp = []
+                for i in LastPlayerCards:
+                    temp.append(i)
+                if set(temp) == set(lastPlayerClaim['Claim_rank']):
                     # Doubt Failed
                     currentPlayer.insert_cards(board.GameBoard.GetDisplayArea)
-                    #board.GameBoard.delDiscard(board.GameBoard.GetDisplayArea)
                     board.GameBoard.ClearDisplay()
-                    # ****PASS SITUATION****
                     return self.players[(self.players.index(currentPlayer) + 1) % len(self.players)]
                 else:
                     # Doubt Succeeded
-                    lastPlayer = self.players[(self.players.index(currentPlayer) - 1)] if \
-                        self.players.index(currentPlayer) else self.players[-1]
+                    lastPlayer = self.players[self.players.index(currentPlayer) - PassCount]
                     lastPlayer.insert_cards(board.GameBoard.DisplayArea)
-                    #board.GameBoard.delDiscard(board.GameBoard.DisplayArea)
                     board.GameBoard.ClearDisplay()
                     return currentPlayer
-            elif (playerInfo['Pass']):
-                return self.players[(self.players.index(currentPlayer) + 1) % len(self.players)]
-            elif (playerInfo['Follow']):
+            elif playerInfo['Choice'] == 'Pass':
+                PassCount += 1
+                if PassCount == len(self.Players) - 1:
+                    return currentPlayer
+
+            elif (playerInfo['Choice'] == 'Follow'):
                 # or claim
                 # Actual Played Card are different from what he claimed*******
-                #currentPlayer.play_cards(playerInfo['Claim']['Claim_rank'])
-                board.GameBoard.Display(playerInfo['Claim']['Claim_rank'])
-                board.GameBoard.Discard(playerInfo['Claim']['Claim_rank'])
+                currentPlayer.play_cards(playerInfo['Cards'])
+                board.GameBoard.Display(playerInfo['Cards'])
                 lastPlayerClaim = playerInfo['Claim']
-                return self.players[(self.players.index(currentPlayer) + 1) % len(self.players)]
+                PassCount = 0
 
     def run_game(self):
         self.reset_game()
@@ -100,7 +106,6 @@ class Game():
     def reset_game(self):
         '''
         Reset the game to its initial state
-
         Steps of reset progress are as follows:
             1.Reset pubilc card Piles
             2.Deal cards to players
@@ -113,7 +118,7 @@ class Game():
         for hand in self.board.Deal(cardsPerPlayer, len(self.players)):
             self.players[index].initial_cards(hand)
             index += 1
-        return
+        return 
 
 if __name__ == "__main__":
     Game()
