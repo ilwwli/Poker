76c9d9763304079fb7e1f674c57dc15031a9da7f
ilwwwli <863886245@qq.com>
2017-12-05 19:47:19 +0800

    CANNOT POST

--- Code/Front/application/views.py
+++ Code/Front/application/views.py
@@ -1,9 +1,9 @@
 from flask import flash, redirect, render_template, \
-     request, url_for
+     request, url_for, session
 from application import app
 
 app.user_name = "admin"
-app.secret_key = 'daydayup' 
+app.secret_key = "admin"
 
 # --- Login Page ---
 @app.route('/login', methods=['GET', 'POST'])
@@ -11,20 +11,28 @@ def login():
     error = None
     if request.method == 'POST':
         if request.form['username'] != app.user_name or \
-                request.form['password'] != app.secret_key:
-            error = 'Invalid credentials'
+                request.form['password']  != app.secret_key:
+            error = 'Invalid User'            
         else:
-            flash('You were successfully logged in')
+            flash('You were successfully logged in', category='error')
+            session['username'] = request.form['username']
             return redirect(url_for('play'))
     return render_template('login.html', error=error)
 
 # --- Game Page ---
 @app.route('/play', methods = ['GET', 'POST'])
 def play():
+    if request.method == 'POST':
+        print(request.form['cards'])
+        flash('Submit Got', category='error')
+        return render_template('cards.html')
+    if 'username' not in session:
+        flash('Skip Login', category='error')        
+        return redirect(url_for('login'))
     cards = ['h5','h6','da','h5','h8','wangsmall','s8','d4','s8','d4','s8','d4','s8','d4'] 
     for card in cards:
         imagesrc = ("../static/pokerimg/%s.jpg " % card)
-        flash(imagesrc,category='cards')
+        flash(imagesrc, category='cards')
     return render_template('cards.html')
 
 # --- Home Page ---
