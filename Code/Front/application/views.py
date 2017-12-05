4797dc1337bc32cfa54861e1fcc6302c55644c7c
ZXH <863886245@qq.com>
2017-12-05 13:28:21 +0800

    New Start

--- Code/Front/application/views.py
+++ Code/Front/application/views.py
@@ -2,28 +2,35 @@ from flask import flash, redirect, render_template, \
      request, url_for
 from application import app
 
-app.secret_key = 'daydayup'
+app.user_name = "admin"
+app.secret_key = 'daydayup' 
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
+# --- Login Page ---
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
     if request.method == 'POST':
-        if request.form['username'] != 'admin' or \
+        if request.form['username'] != app.user_name or \
                 request.form['password'] != app.secret_key:
             error = 'Invalid credentials'
         else:
             flash('You were successfully logged in')
-            return redirect(url_for('index'))
+            return redirect(url_for('play'))
     return render_template('login.html', error=error)
 
+# --- Game Page ---
 @app.route('/play', methods = ['GET', 'POST'])
 def play():
-    cards = ['h5','h6','da']    
+    cards = ['h5','h6','da','h5','h8','wangsmall','s8','d4','s8','d4','s8','d4','s8','d4'] 
     for card in cards:
         imagesrc = ("../static/pokerimg/%s.jpg " % card)
         flash(imagesrc,category='cards')
     return render_template('cards.html')
+
+# --- Home Page ---
+@app.route('/', methods=['GET'])
+@app.route('/index', methods=['GET'])
+#@auth.login_required
+def get_tasks():
+   # return jsonify({'tasks': map(make_public_task, tasks)})
+    return render_template('index.html')
